74e578cc71dd456bfcc7128000055462133cd223
OBCL <patrickdias3630@gmail.com>
2024-07-23 00:44:55 +0200

    Removed useless files and set different speeds for crouching and standing

--- Main/Scripts/Player.gd
+++ Main/Scripts/Player.gd
@@ -2,15 +2,15 @@ extends CharacterBody2D
 
 @onready var PlayerAnim = $CameraSmoothing/PlayerAnimatedSprite
 @onready var Cshape = $Collisions
-@onready var Camera = $Camera
-@onready var Player = $"."
 
 @export var SPEED = 250
+@export var CROUCH_SPEED = 100
 @export var JUMP_POWER = -350
 @export var GRAVITY = 700
 @export var FALL_GRAVITY = 1200
 @export var FAST_FALL_GRAVITY = 3700
 
+var Crouching = false
 var Jump_Buffer = false
 var was_on_floor
 
@@ -22,10 +22,12 @@ func _physics_process(delta):
 	if !is_on_floor():
 		velocity.y += get_gravity(velocity) * delta
 	
-	Jump()
+	if !Crouching:
+		Jump()
+	
+	Look_up()
 	get_input()
 	Crouch()
-	Look_up()
 	
 	if !was_on_floor && is_on_floor():
 		if Jump_Buffer:
@@ -38,15 +40,15 @@ func _physics_process(delta):
 func get_input():
 	var direction = Input.get_axis("Move_Left","Move_Right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * get_speed()
 	else:
-		velocity.x = direction * -SPEED
+		velocity.x = direction * -get_speed()
 
 func Look_up():
 	if Input.is_action_pressed("Look_Up"):
-		Camera.position.y = -100
+		$Camera.position.y = -100
 	else:
-		Camera.position.y = 0
+		$Camera.position.y = 0
 
 func Crouch():
 	if Input.is_action_pressed("Crouch") and is_on_floor():
@@ -54,11 +56,13 @@ func Crouch():
 		PlayerAnim.position.y = 5
 		Cshape.shape = Crouching_Cshape
 		Cshape.position.y = 5
+		Crouching = true
 	else:
 		PlayerAnim.play("IDLE")
 		PlayerAnim.position.y = 0
 		Cshape.shape = Standing_Cshape
 		Cshape.position.y = 0
+		Crouching = false
 
 
 func Jump():
@@ -73,6 +77,11 @@ func Jump():
 	if Input.is_action_just_released("Jump"):
 		if !is_on_floor():
 			velocity.y /= 4
+
+func get_speed():
+	if !Crouching:
+		return SPEED
+	return CROUCH_SPEED 
 
 func get_gravity(velocity: Vector2):
 	if velocity.y <= 0:
