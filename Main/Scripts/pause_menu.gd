4cdd99061c12dfb374a4f41d3d269a97e6c44d0b
OBCL <patrickdias3630@gmail.com>
2024-08-07 13:45:44 +0200

    same thing as last update

--- Main/Scripts/pause_menu.gd
+++ Main/Scripts/pause_menu.gd
@@ -24,7 +24,7 @@ func _on_resume_pressed():
 
 func _on_save_pressed():
 	Utils.Save_Game()
-	Resume()
+	
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Main/Scenes/main_menu.tscn")
